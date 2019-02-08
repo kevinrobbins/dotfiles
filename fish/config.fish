2b7d0e6ef673250d09fdd1dd4075de689eb467e9
Kevin Robbins <kevin.robbins99@gmail.com>
2019-02-08 12:53:53 -0500

    initial commit

---
+++ fish/config.fish
@@ -0,0 +1,33 @@
+# powerline
+set REPOSITORY_ROOT "/home/krobbins/.local/lib/python2.7/site-packages"
+set fish_function_path $fish_function_path "$REPOSITORY_ROOT/powerline/bindings/fish"
+#powerline-setup
+
+# virtualfish
+eval (python -m virtualfish)
+
+# make vim the default editor
+if not set -q EDITOR
+  export EDITOR="vim"
+end
+
+# start vim in INSERT mode
+alias vim "vim +star"
+
+##### start up #####
+
+# start tmux
+set SESSION_NAME default
+begin
+   not set -q TMUX
+   and tmux new-session -As $SESSION_NAME
+end
+
+# Enable sites virtual environment by default
+if not set -q VIRTUAL_ENV
+   vf activate sites
+end
+
+
+# Navigate to working directory
+cd ~/git/nimbis/sites
