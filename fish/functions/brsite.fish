bdfd3efae1d5670e920f9fec179bbe81b244fd8f
Kevin Robbins <kevin.robbins99@gmail.com>
2020-02-05 18:05:43 -0500

    Activate correct virtual environment for various nimbis functions
    These all worked if in either virtual environment, but that's not
    guaranteed in the future. This will ensure that the commands will run
    even if there is any incompatibility introduced in the future.

--- fish/functions/brsite.fish
+++ fish/functions/brsite.fish
@@ -1,5 +1,22 @@
 # backup and restore the specified site
 function brsite
+    # if there's a virtual environment set when this is run, save the name of
+    # it so we can re-activate it when we're done.
+    if set -q VIRTUAL_ENV
+        set orig_venv (string split "/" -- $VIRTUAL_ENV)[-1]
+    end
+
+    # activate sysop venv
+    vf activate sysop
+
+    # run backup and restore commands against the provided site
     make -C $HOME/git/nimbis/sysop/ansible backup SITE=$argv
     make -C $HOME/git/nimbis/sysop/ansible restore SITE=$argv
+
+    # reactivate original venv if there was one
+    if set -q orig_venv
+        vf activate $orig_venv
+    else
+        vf deactivate sysop
+    end
 end
--- fish/functions/runsite.fish
+++ fish/functions/runsite.fish
@@ -1,5 +1,6 @@
-# run migrations for the specified site
-function msite
+# run the specified site
+
+function runsite
     # if there's a virtual environment set when this is run, save the name of
     # it so we can re-activate it when we're done.
     if set -q VIRTUAL_ENV
@@ -9,8 +10,8 @@ function msite
     # activate sites venv
     vf activate sites
 
-    # run migrate command against the provided site
-    make -C $HOME/git/nimbis/sites migrate SITE=$argv
+    # run the run site command against the provided site
+    make -C $HOME/git/nimbis/sites run SITE=$argv
 
     # reactivate original venv if there was one
     if set -q orig_venv
