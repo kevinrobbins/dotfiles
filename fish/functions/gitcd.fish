fae803fd28ebe8b2dde522be2d770fe1984f12f4
Kevin Robbins <kevin.robbins99@gmail.com>
2021-03-19 00:27:18 -0400

    Add gitcd fish function
    This allows me to drop into any repository from anywhere. Since I track
    the gitlab group hierarchy, adding to CDPATH doesn't scale as I would
    need to keep adding paths. The drawback to this is there are a handful
    of repos that share the same name. This causes some to be unreachable. I
    also would like this to have autocomplete, but hey maybe that's for a
    rainy day.

---
+++ fish/functions/gitcd.fish
@@ -0,0 +1,21 @@
+# navigate to git repository
+# Looks for the following values in order:
+#    -r/--repo-root argument
+#    REPO_ROOT environment variable
+#    ~/git/ default
+
+function gitcd --argument-names repo_name repo_root --description "Navigate to specified git repository"
+    argparse --min-args 1 --max-args 1 'r/repo_root=' -- $argv
+
+    set repo $argv
+    set max_depth 6
+    if set -q _flag_repo_root
+        set repo_root $_flag_repo_root
+    else if set -q REPO_ROOT
+        set repo_root $REPO_ROOT
+    else
+        set repo_root ~/git/
+    end
+
+    cd (find $repo_root -maxdepth $max_depth -name $repo -exec test -d {}/.git \; -print -prune -quit)
+end
