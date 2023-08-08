226ad4a966a87d4424702cf1cef2c2634689ed71
Kevin Robbins <kevin.robbins99@gmail.com>
2023-08-08 12:48:15 -0500

    Add fish abbreviations to config.fsh
    Fish 3.6.0 stopped supporting abbreviations stored in universal variables.
    Existing abbreviations will continue to work, but no new ones can be added. To
    avoid issues in the future, add all abbreviations to config.fish.

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,32 @@
+## Abbreviations ##
+
+# Virtual Environments
+abbr --add pvn mkvirtualenv
+abbr --add pvd deactivate
+abbr --add pva activate_venv
+
+# Git
+abbr --add gits git status
+abbr --add gita git add
+abbr --add gitc git commit
+abbr --add gitcv git commit --verbose
+abbr --add gitco git checkout
+abbr --add gitpu git pull
+abbr --add gitps git push
+abbr --add gitrb git rebase
+abbr --add gitrbi git rebase --interactive
+abbr --add gitdu git diff @{u}
+abbr --add gitg git log --graph --oneline --all
+
+# Docker compose
+abbr --add dc docker compose
+abbr --add dcp docker compose --profile
+
+# ls
+abbr --add ls ls -al
+
+## Config ##
+
 # make vim the default editor
 if not set -q EDITOR
     export EDITOR="vim"
